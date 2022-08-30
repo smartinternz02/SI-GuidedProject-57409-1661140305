e321d23ec0d18b9fe741ab298cda6eecfcd2c6a9
Hareesh Naik <90563881+Hareesh061@users.noreply.github.com>
2022-08-30 17:34:16 +0530

    Add files via upload

---
+++ android-basics-kotlin-forage-app/app/src/main/java/com/example/forage/data/ForageDatabase.kt
@@ -0,0 +1,12 @@
+
+package com.example.forage.data
+
+import androidx.room.RoomDatabase
+import com.example.forage.model.Forageable
+
+/**
+ * Room database to persist data for the Forage app.
+ * This database stores a [Forageable] entity
+ */
+// TODO: create the database with all necessary annotations, methods, variables, etc.
+abstract class ForageDatabase : RoomDatabase()
